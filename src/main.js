539d9ae20186f3bee2fef3b7da4931f2debeb6c8
kazz <kazzzz1001@gmail.com>
2020-12-12 11:45:54 +0900

    routing

--- src/main.js
+++ src/main.js
@@ -2,10 +2,18 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import App from "./App.vue";
 import Register from "./pages/Register.vue";
+import RegisterResult from "./pages/RegisterResult.vue";
+import Gacha from "./pages/Gacha.vue";
+import GachaResult from "./pages/GachaResult.vue";
 
 Vue.use(VueRouter);
 
-const routes = [{ path: "/register", component: Register }];
+const routes = [
+  { path: "/register", component: Register },
+  { path: "/register_result", component: RegisterResult },
+  { path: "/gacha", component: Gacha },
+  { path: "/gacha_result", component: GachaResult },
+];
 const router = new VueRouter({
   routes,
 });
--- src/pages/GachaResult.vue
+++ src/pages/GachaResult.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    GachaResult
+    <label></label>
   </div>
 </template>
 
